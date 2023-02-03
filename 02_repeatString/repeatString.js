384121c2382824df586ecd9b65f124f5669685f7
Yurim Kang <yoorim7480@gmail.com>
2023-02-03 17:30:06 -0500

    repeats string n times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    result = '';
+    if (num < 0) {
+        return 'ERROR';
+    }else {
+        for (i = 0; i < num; i++) {
+            result += word;
+        }
+        return result;
+    }
 };
 
 // Do not edit below this line
