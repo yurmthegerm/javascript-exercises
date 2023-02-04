1af6d7a2d911938f4c2b86957f658ea5561f0bf1
Yurim Kang <yoorim7480@gmail.com>
2023-02-04 18:08:00 -0500

    different than solution but working fine

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(no, nt) {
+    let array = [];
+    let sum = 0;
+    if (Number.isInteger(no) && Number.isInteger(nt) && no > 0 && nt > 0) {
+        if (no < nt){
+            array = [no,nt];
+        } else {
+            array = [nt,no];
+        }
+        let num = array[0];
+        do {
+            sum += num;
+            num ++;
+        } while (num <= array[1]);
+        return sum;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
