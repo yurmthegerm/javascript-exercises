72947a7194cd4de1b4da442ae887e6b89b017d87
Yurim Kang <yoorim7480@gmail.com>
2023-02-04 17:43:01 -0500

    wrong implementation

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(lst, ...rest) {
+    let result = lst;
+    for (const arg of lst) {
+        for (const elem of rest) {
+            if (arg === elem) {
+                result.splice(result.indexOf(arg),1);
+            }
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
