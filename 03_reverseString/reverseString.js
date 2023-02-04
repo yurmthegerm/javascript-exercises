ff60e7c9c4cfd80f461ac345bbc70e6401944926
Yurim Kang <yoorim7480@gmail.com>
2023-02-04 18:10:06 -0500

    idk if i did it

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split('').reverse().join('');
+    /*
+    let result = '';
+    if (string === '') {
+        return '';
+    }else {
+        for (i = string.length-1; i <= 0; i --) {
+            result += string.charAt(i);
+        }
+    }
+    return result;
+    */
 };
 
 // Do not edit below this line
