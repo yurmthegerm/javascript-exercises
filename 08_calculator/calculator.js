c10eba219a4adfcac7653c6e70931599ffb11e93
Yurim Kang <yoorim7480@gmail.com>
2023-03-18 17:57:02 -0400

    modified using solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,64 @@
-const add = function() {
-	
+const add = function(a, b) {
+  b = b || 0; //optional param
+  const numO = parseInt(a);
+  const numT = parseInt(b);
+  const sum = numO + numT;
+	return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	const numO = parseInt(a);
+  const numT = parseInt(b);
+  const sub = numO-numT;
+	return sub;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  /*
+	let result = 0; 
+  if (Array.isArray(a)) { //if a is an array
+    if (a.length > 0) { //if a stores any value
+      for (let i = 0; i < a.length; i++) {
+        result += parseInt(a[i]); //add all elems to sum
+      }
+    } else { //if a is an empty array
+      return result; 
+    }
+  }
+  return result;
+  */
+  return a.reduce((total, current) => total+current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  /*
+  let result = 1;
+  for (let i = 0; i < a.length; i++) {
+    result *= a[i];
+  }
+  return result;
+  */
+  return a.length 
+    ? a.reduce((total, current) => total * current)
+    : 0;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return (a**b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let num = parseInt(a);
+  let result = 1;
+  let count = num;
+  if (num != 1 && num != 0) {
+    while (count != 0) {
+      result *= num;
+      num --;
+      count --;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
