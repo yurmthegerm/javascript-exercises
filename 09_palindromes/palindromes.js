fe4b9019d4ef895e4d97f55f7441b688b35c1d2c
Yurim Kang <yoorim7480@gmail.com>
2023-03-18 18:11:04 -0400

    got help from solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const thing = str.toLowerCase().replace(/[^a-z]/g, "");
+    return thing == thing.split("").reverse().join("");
 };
 
 // Do not edit below this line
