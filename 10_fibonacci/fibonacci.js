c1d91f5cb1a7c5126f163dd2a6456d5d5e75910c
Yurim Kang <yoorim7480@gmail.com>
2023-04-08 18:57:32 -0400

    done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const order = parseInt(num);
+    if (order < 0) {
+        return 'OOPS';
+    } else if (order === 0) {
+        return 0;
+    } else {
+        let a = 0;
+        let b = 1;
+        for (let i = 1; i < order; i++) {
+            const temp = b;
+            b = a + b;
+            a = temp;
+        }
+        return b;
+    }
 };
 
 // Do not edit below this line
